012728c28aad0d34204e3fa3e982e18aaded303d
rla03 <Rajat.Acharya@fullstacklabs.ca>
2021-09-14 14:34:14 -0230

    JavaIOTest created to split the data from bank_transactions.txt and able to date, and its respective day and DAY_OF_WEEK.

--- Assignment1/src/io/JavaIOTest.java
+++ Assignment1/src/io/JavaIOTest.java
@@ -5,19 +5,24 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
 public class JavaIOTest {
 
     public static void main(String[] args) throws IOException, ParseException {
+
+        String[] date = null;
+        String finalDay = null;
+        String check = "Monday";
+        Calendar c = Calendar.getInstance();
+
         // Read from file
         File f1 = new File("/Users/rajatacharya/Documents/Bo-Ca-Aug-2021/boca-aug21-assignment-1/Assignment1/tmp/bank_transactions.txt");
         FileReader fr = new FileReader(f1);
         BufferedReader br = new BufferedReader(fr);
 
         String read = br.readLine();
-        String[] date = null;
-        String finalDay = null;
 
         // Write from file
         File f2 = new File("/Users/rajatacharya/Documents/Bo-Ca-Aug-2021/boca-aug21-assignment-1/Assignment1/tmp/out.txt");
@@ -28,19 +33,27 @@ public class JavaIOTest {
         while (read != null) {
             date = read.split("\t");
             String t = date[1];
+
             SimpleDateFormat format1 = new SimpleDateFormat("dd-MMM-yy");
             Date dt1 = format1.parse(t);
             DateFormat format2 = new SimpleDateFormat("EEEE");
             finalDay = format2.format(dt1);
-            // System.out.println("Day is :" + finalDay);
+            System.out.println("Day is : " + finalDay);
 
-            bw.write(date[1] + " " + finalDay);
-            bw.newLine();
-            System.out.println(date[1] + " " + finalDay);
-            read = br.readLine();
-            System.out.println(Arrays.toString(date));
+            c.setTime(dt1);
+            int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+            System.out.println("Day of week: " + dayOfWeek);
+
+            System.out.println("equal or not check with Monday " + finalDay.equals(check));
+
+            //if (dayOfWeek == 2) {
+                bw.write(date[1] + " " + finalDay + " " +dayOfWeek);
+                bw.newLine();
+                System.out.println(date[1] + " " + finalDay);
+                read = br.readLine();
+                System.out.println(Arrays.toString(date));
+         //  }
         }
-
         bw.flush();
         bw.close();
     }
